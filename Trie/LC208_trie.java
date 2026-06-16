8017873aa912efc3002e379721fefb7b70174ff4
Naitik <naitik242424@gmail.com>
2026-06-16 20:34:07 +0530

    added trie

---
+++ Trie/LC208_trie.java
@@ -0,0 +1,62 @@
+package DSA.Trie;
+
+public class LC208_trie {
+    static class Node {
+        Node[] children;
+        boolean isEnd;
+
+        public Node() {
+            children = new Node[26];
+            for (int i = 0; i < 26; i++) {
+                children[i] = null;
+            }
+            isEnd = false;
+        }
+    }
+
+    static Node root = new Node();
+
+    public static void insert(String word) {
+        Node curr = root;
+        for (int i = 0; i < word.length(); i++) {
+            int idx = word.charAt(i) - 'a';
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            }
+            if (i == word.length() - 1) {
+                curr.children[idx].isEnd = true;
+
+            }
+            curr = curr.children[idx];
+        }
+        curr.isEnd = true;
+
+    } 
+
+    public static boolean search(String key) {
+        Node curr = root;
+        for (int i = 0; i < key.length(); i++) {
+            int idx = key.charAt(i) - 'a';
+            Node node = curr.children[idx];
+            if (node == null) {
+                return false;
+            }
+            if (i == key.length() - 1 && node.isEnd == false) {
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return true;
+
+    }
+
+    public static void main(String[] args) {
+        String words[] = { "the", "a", "there", "their", "any" };
+        for (int i = 0; i < words.length; i++) {
+            insert(words[i]);
+        }
+        System.out.println(search("their"));
+        System.out.println(search("a"));
+    }
+
+} 
