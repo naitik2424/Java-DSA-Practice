76a597f901323590c9b76e19de2f8c39fbc37f44
Naitik <naitik242424@gmail.com>
2026-07-28 00:48:03 +0530

    union of two sorted arrays

---
+++ array/Union.java
@@ -0,0 +1,30 @@
+package array;
+
+import java.util.*;
+
+public class Union {
+
+    public static ArrayList<Integer> union(int[] arr1, int[] arr2){
+    HashSet<Integer> set = new HashSet<>();
+    for(int i=0;i<arr1.length;i++){
+        set.add(arr1[i]);
+    }
+    for(int i=0;i<arr2.length;i++){
+        set.add(arr2[i]);
+    }
+    ArrayList<Integer> ans = new ArrayList<>();
+    for(int num : set){
+        ans.add(num);
+    }
+    Collections.sort(ans);
+    return ans;
+
+    }
+    public static void main(String[] args){
+        int[] arr1 = {1,2,2,3,4};
+        int[] arr2= {2,3,4,4,5,6};
+        ArrayList<Integer> result = union(arr1, arr2);
+        System.out.println(result);
+    }    
+
+}
