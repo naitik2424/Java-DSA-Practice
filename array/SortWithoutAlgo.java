fde214b9d2e8f03728da0fac6c9f8fc109a88c0d
Naitik <naitik242424@gmail.com>
2026-08-01 12:46:37 +0530

    medium level Arrays

--- array/SortWithoutAlgo.java
+++ array/SortWithoutAlgo.java
@@ -3,14 +3,29 @@ package array;
 public class SortWithoutAlgo {
     public static void main(String[] args){
         int[] arr = {2,1,0,1,0,2};
-        int n = arr.length;
-        int low =0,mid=0,high =n;
-        while(mid<high){
+        int n = arr.length-1;
+        int low =0;
+        int mid=0;
+        int high =n;
+        while(mid<=high){
             if(arr[mid]==0){
                 int temp = arr[mid];
                 arr[mid] = arr[low];
                 arr[low] = temp;
+                low++;
+                mid++;
+            }else if(arr[mid]==1){
+                mid++;
+            }else{
+                int temp = arr[high];
+                arr[high]=arr[mid];
+                arr[mid] =temp;
+                high--;
+
             }
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
         }
     }
     
--- array/TwoSum.java
+++ array/TwoSum.java
@@ -20,14 +20,41 @@ public class TwoSum {
 
         return new int[]{-1, -1};
     }
+     public static int[] twoSum2(int[] nums,int target){
+        int n = nums.length;
+        int left =0,right=n-1;
+        for(int i=0;i<n;i++){
+            while(left<right){
+                if(nums[left]+nums[right] == target){
+                    return new int[]{left,right};
+                }
+                else if(nums[left]+nums[right]<target){
+                    left++;
+                }else{
+                    right--;
+
+                }
+            }
+        }
+        return new int[]{-1,-1};
+
+     }
 
     public static void main(String[] args) {
 
         int[] nums = {2, 7, 11, 15};
         int target = 26;
 
-        int[] result = twoSum(nums, target);
+        int[] result = twoSum2(nums, target);
 
         System.out.println(result[0] + " " + result[1]);
     }
 }
+
+
+
+// t.c = o(n)  unordered_map 
+// t.c = o(nlogn)  sort + two pointer optimized one (twoSum2)
+// t.c = o(n^2)  brute force or in worst case
+// s.c = o(n)  unordered_map
+// s.c = o(1)  two pointer optimized one (twoSum2)
