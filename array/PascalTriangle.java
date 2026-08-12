6ffe0de26326d20eb63f7e683e640752287fc68f
Naitik <naitik242424@gmail.com>
2026-08-13 01:24:37 +0530

    pascal'sTriangle

--- array/PascalTriangle.java
+++ array/PascalTriangle.java
@@ -1,7 +1,31 @@
 package array;
 
-public class PascalTriangle {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.List;
+
+class PascalTriangle {
+    public static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i=0;i<numRows;i++){
+            List<Integer> row=new ArrayList<>();
+            for(int j=0;j<=i;j++){
+                if(j==0 || j==i){
+                    row.add(1);
+                }
+                else{
+                    int value=ans.get(i-1).get(j-1)+ans.get(i-1).get(j);
+                    row.add(value);
+                }
+            }
+            ans.add(row);
+        }
+        return ans;
+
         
+    }
+
+    public static void main(String[] args) {
+        int numRows = 5;
+        System.out.println(generate(numRows));
     }
 }
