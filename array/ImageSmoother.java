a5f26ed5e134560affa191c00ad689506069f203
Naitik <naitik242424@gmail.com>
2026-08-20 02:09:43 +0530

    updated ImgSmoother

--- array/ImageSmoother.java
+++ array/ImageSmoother.java
@@ -3,12 +3,12 @@ public class ImageSmoother {
     public static int[][] imageSmoother(int[][] img){
         int n=img.length;
         int m=img[0].length;
-        int sum=0,count=0;
         int[][] mat = new int[n][m];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
+                int sum=0,count=0;
                 for(int x=i-1;x<=i+1;x++){
-                    for(int y=j-1;y<=j+1;j++){
+                    for(int y=j-1;y<=j+1;y++){
                         if(x>=0 && x<n && y>=0 && y<m){
                             sum+=img[x][y];
                             count++;
@@ -24,6 +24,12 @@ public class ImageSmoother {
     }
     public static void main(String[] args) {
         int[][] img= {{100,200,300},{200,50,200},{100,200,100}};
-        System.out.println(imageSmoother(img));
+        int[][] ans =imageSmoother(img);
+        for(int i=0;i<ans.length;i++){
+            for(int j=0;j<ans[0].length;j++){
+                System.out.print(ans[i][j]+ " ");
+            }
+            System.out.println();
+        }
     }
 }
